7ee31c992e470e9e75785c4c12e9588fa79bea60
Rosema <rosemashak789@gmail.com>
2023-06-20 06:47:52 +0545

    Nav bar added

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -1,12 +1,12 @@
 import 'popper.js';
-// import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/dropdown';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/tab';
-// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/popover';
 // import 'bootstrap-select';
-// import 'bootstrap/js/dist/modal';
 
 
 class global {
